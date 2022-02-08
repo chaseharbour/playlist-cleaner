9939e6af264daea914b2c673dd2f65dc5878d5b3
chaseharbour <harbourcp@gmail.com>
2022-02-08 14:31:49 -0800

    prog(knex, docker-compose): Began setting up knex, issue with connecting to db using knex.

---
+++ server/knexfile.js
@@ -0,0 +1,43 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgresql://postgres:pass@pg:5432/db",
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
---
+++ server/migrations/20220208212608_create-users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTables("users", (table) => {
+    table.increments();
+    table.string("username");
+    table.string("spotifyId");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
